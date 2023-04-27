2a32617fb6380a5919064133e169a4b317947a46
Xylusca <lukas_sud@live.com>
2023-04-27 17:36:27 -0300

    namespace

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse('CONTATO')
