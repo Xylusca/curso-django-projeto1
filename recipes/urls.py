f3e0aa90c611839172751eb93ea480656d9c6673
Xylusca <lukas_sud@live.com>
2023-04-30 18:00:11 -0300

    urls dynamics

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recepis/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recepis/<int:id>/', views.recipe, name="recipe"),
 
 
 ]
