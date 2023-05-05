b7a7c008a3acc32e4ad391d0c0e49990685fa80f
Xylusca <lukas_sud@live.com>
2023-05-05 18:22:35 -0300

    rename projeto to project

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for projeto project.
+project URL configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
