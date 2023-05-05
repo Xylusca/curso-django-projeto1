e803266416e5a7aa9534d04a1d5de5e86fe09918
Xylusca <lukas_sud@live.com>
2023-05-05 19:39:24 -0300

    test url home recipe

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class recipeUrlsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        variavel = '123456'
-        print('Olá teste')
-        assert 1 == 1
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
