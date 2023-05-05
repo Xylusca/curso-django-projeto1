b740d3493a142645403bb57382ec522990a7b12d
Xylusca <lukas_sud@live.com>
2023-05-05 19:18:38 -0300

    test start

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class recipeUrlsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        variavel = '123456'
+        print('Olá teste')
+        assert 1 == 1
