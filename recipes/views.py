b1ada431aee09cae228356613f1488d14c2c676f
Xylusca <lukas_sud@live.com>
2023-05-15 18:40:36 -0300

    search test

--- recipes/views.py
+++ recipes/views.py
@@ -31,3 +31,7 @@ def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     context = {'recipe': recipe, 'is_detail_page': True}
     return render(request, 'recipes/pages/recipe-view.html', context=context)
+
+
+def search(request):
+    return render(request, 'recipes/pages/search.html')
