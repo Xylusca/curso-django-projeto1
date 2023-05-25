269480b9d33585c0a241288606766b81e3e60c40
Lucas Pereira <lukas_sud@live.com>
2023-05-25 15:30:17 -0300

    css template and form add

--- authors/forms.py
+++ authors/forms.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(forms.ModelForm):
+    confirama_password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password',
+            'class': 'form-control',
+            'label': 'Repeat your password',
+        })
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,3 +21,25 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: Lucas'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: Pereira'
+            }),
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Your username'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Your e-mail'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Your password'
+            }),
+        }
