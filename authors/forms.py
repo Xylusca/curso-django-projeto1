8fa46970e2132c3a183e1d1ec3e3fe0d86549b3b
Lucas Pereira <lukas_sud@live.com>
2023-05-26 16:39:09 -0300

    ValidationError password

--- authors/forms.py
+++ authors/forms.py
@@ -1,15 +1,49 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+    if not regex.match(password):
+        raise ValidationError((
+            'The password must have at least: '
+            'One uppercase letter, '
+            'One lowercase letter, '
+            'One number, '
+            'Eight characters.'
+        ),
+            code='invalid'
+        )
 
 
 class RegisterForm(forms.ModelForm):
-    confirama_password = forms.CharField(
+    password2 = forms.CharField(
         required=True,
         widget=forms.PasswordInput(attrs={
             'placeholder': 'Repeat your password',
             'class': 'form-control',
             'label': 'Repeat your password',
         })
+    )
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Your password',
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+        validators=[strong_password]
     )
 
     class Meta:
@@ -38,8 +72,18 @@ class RegisterForm(forms.ModelForm):
                 'class': 'form-control',
                 'placeholder': 'Your e-mail'
             }),
-            'password': forms.PasswordInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Your password'
-            }),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            raise ValidationError({
+                'password':
+                'The entered passwords do not match. Please try again.',
+                'password2':
+                'The entered passwords do not match. Please try again.',
+            })
